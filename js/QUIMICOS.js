99ceb1129d06dc2a0024675dd8b4682fdbc46d6c
leonardo <leonardo2322@gmail.com>
2022-08-15 17:19:03 -0500

    cambios en quimicos

---
+++ js/QUIMICOS.js
@@ -0,0 +1,33 @@
+const contFather = document.getElementById("Father")
+
+let datosDelJson = [{"id":556,"Productos":"SELLADOR ADESIVO DE SILICONA LOCTITE","seccion":"QUIMICOS","Precios":35000},{"id":555,"Productos":"LUBRICANTE 5-56 CRC","seccion":"QUIMICOS","Precios":25000},{"id":310,"Productos":"SOLDADURA EXOTERMICA","seccion":"QUIMICOS","Precios":36000}]
+
+document.addEventListener('DOMContentLoaded',function () {
+    for (const iterator of datosDelJson) {
+        let contendedorCards = document.createElement('section')
+
+        let div = `<div class="slide-container" data-id="0">
+                        <img class="back-card" src="../img/SimulacionPlomoMinimercadolaEsperanza (1).png" alt="">
+                    <div class="slide">
+                        <div class="content">
+                        <span class="spanP">codigo: ${iterator.id}</span>
+                        <h3 class="amarilla">${iterator.Productos}</h3>
+    
+                    </div>
+                 <div class="image">
+                    <img class="img-conTornillos" src="../img/images/TERMINAL HEMBRA AZUL.png"
+                    class="shoe" alt="Chunky">
+                </div>
+            </div>
+        `
+        console.log(iterator)
+
+
+        contendedorCards.innerHTML = div
+        contFather.append(contendedorCards)
+
+
+}
+})
+
+
