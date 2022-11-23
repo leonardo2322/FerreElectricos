de0a3371802607e77191d4af55086664ac33a73d
leonardo <leonardo@localhost.localdomain>
2022-11-22 20:56:36 -0500

    cambios de imagen

--- js/jsAcrilico.js
+++ js/jsAcrilico.js
@@ -1,7 +1,7 @@
 const contFather = document.getElementById("Father")
 
-let datosDelJson = [{"id":220,"Productos":"ACRILICO GRANDE ","seccion":"ACRILICO","Precios":35000,"image":"../img/images/acrilico/ACRILICO GRANDE .png"},
-{"id":430,"Productos":"ACRILICO MEDIANO","seccion":"ACRILICO","Precios":10000,"image":"../img/images/acrilico/ACRILICO GRANDE .png"},{"id":219,"Productos":"ACRILICO PEQUE\u00d1O","seccion":"ACRILICO","Precios":8000,"image":"../img/images/acrilico/ACRILICO .png"},{"id":857,"Productos":"PINTURA BROCA TINER","seccion":"ACRILICO","Precios":30800,"image":"../img/images/acrilico/PINTURA BROCA TINER.png"},{"id":855,"Productos":"TAPA LAMINADA","seccion":"ACRILICO","Precios":198000,"image":"../img/images/acrilico/TAPA LAMINADA.png"}]
+let datosDelJson = [{"id":220,"Productos":"ACRILICO GRANDE ","seccion":"ACRILICO","Precios":35000,"image":"../img/images/acrilico/a220.jpg"},
+{"id":430,"Productos":"ACRILICO MEDIANO","seccion":"ACRILICO","Precios":10000,"image":"../img/images/acrilico/a220.jpg"},{"id":219,"Productos":"ACRILICO PEQUE\u00d1O","seccion":"ACRILICO","Precios":8000,"image":"../img/images/acrilico/a220.jpg"},{"id":857,"Productos":"PINTURA BROCA TINER","seccion":"ACRILICO","Precios":30800,"image":"../img/images/acrilico/PINTURA BROCA TINER.png"},{"id":855,"Productos":"TAPA LAMINADA","seccion":"ACRILICO","Precios":198000,"image":"../img/images/acrilico/TAPA LAMINADA.png"}]
 
 
 document.addEventListener('DOMContentLoaded',function () {
